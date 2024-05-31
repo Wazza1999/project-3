0bff45f2cd629c1d60d555f433a74117e374b60b
Wazza1999 <warren.selby@hotmail.co.uk>
2024-05-31 21:55:30 +0000

    added code to show sales data

--- run.py
+++ run.py
@@ -1,22 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Input Username (Wazza123) and Password (1234) 
-attempts = 0
-
-while attempts < 3:
-    Username = input("Enter your Username:")
-    Password = input("Enter your Password:")
-
-    if Username == 'Wazza123' and Password =='1234':
-        print('Welcome to LeBouchon Wazza123. ')
-        break
-        
-    else:
-        print('Incorrect Credentials. Please try again.')
-        attempts += 1
-        continue
-
 ### SCOPE code
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -32,4 +16,29 @@ SHEET = GSPREAD_CLIENT.open('Le_Bouchon')
 sales = SHEET.worksheet('Sales')
 
 data = sales.get_all_values()
+
+# Input Username (Wazza123) and Password (1234) 
+print("Welcome to Le Bouchon Hotel and Brasseries' sales data.")
+attempts = 0
+
+while attempts < 3:
+    Username = input("Enter your Username:")
+    Password = input("Enter your Password:")
+
+    if Username == 'Wazza123' and Password =='1234':
+        print('Welcome to LeBouchon Wazza123. ')
+        break
+        
+    else:
+        print('Incorrect Credentials. Please try again.')
+        attempts += 1
+        continue
+
+# If yes is selected the program will show the sales data
+user_input = input('Would you like to see the Sales data that is available? y/n:')
+if user_input == 'y':
+    print(data)
+
+else:
+    print("Thank you have a nice day :)")
 
